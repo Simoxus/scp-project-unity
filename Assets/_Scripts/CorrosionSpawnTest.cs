5e95d313a0b7cd1fb69709210c424c7aa09dc21e
Simoxus <simoxusmanagement@gmail.com>
2025-06-04 08:35:10 -0600

    Add corrosion decal spawning

---
+++ Assets/_Scripts/CorrosionSpawnTest.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using Cysharp.Threading.Tasks;
+
+public class CorrosionDecalSpawnTest : MonoBehaviour
+{
+    private void Update()
+    {
+
+    }
+
+    private async UniTaskVoid HandleCorrosionSpawnAsync()
+    {
+
+    }
+}
