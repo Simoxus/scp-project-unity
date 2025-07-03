0918f355ca91ec15f763b6e9a1a9d3f281043cfc
Simoxus <simoxusmanagement@gmail.com>
2025-07-02 23:02:53 -0600

    Fix input controls for Gamepad and refactor events

--- Assets/_Scripts/Player/PlayerInputs.cs
+++ Assets/_Scripts/Player/PlayerInputs.cs
@@ -1,9 +1,10 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.InputSystem;
+using System;
 
 public class PlayerInputs : MonoBehaviour
 {
-	[Header("Input Actions")]
+    [Header("Input Actions")]
     [SerializeField] private InputActionAsset playerInputAsset;
 
     public Vector2 moveInput { get; private set; }
@@ -11,59 +12,114 @@ public class PlayerInputs : MonoBehaviour
     public bool sprintHeld { get; private set; }
     public bool crouchHeld { get; private set; }
     public bool blinkPressed { get; private set; }
+    public bool crouchPressed { get; private set; }
+
+    public event Action OnInteractPressed;
+    public event Action OnPauseInputPerformed;
 
     private InputActionMap _playerActionMap;
+    private InputActionMap _uiActionMap;
+
     private InputAction _moveAction;
     private InputAction _lookAction;
     private InputAction _sprintAction;
     private InputAction _blinkAction;
     private InputAction _crouchAction;
+    private InputAction _pauseAction;
+    private InputAction _interactAction;
 
-    private void Awake() // Cache input actions to get better performance
+    private void Awake()
     {
         _playerActionMap = playerInputAsset.FindActionMap("Player");
+        _uiActionMap = playerInputAsset.FindActionMap("UI");
 
         _moveAction = _playerActionMap.FindAction("Move");
         _lookAction = _playerActionMap.FindAction("Look");
         _sprintAction = _playerActionMap.FindAction("Sprint");
-        _blinkAction = _playerActionMap.FindAction("Crouch");
-        _crouchAction = _playerActionMap.FindAction("Blink");
+        _blinkAction = _playerActionMap.FindAction("Blink");
+        _crouchAction = _playerActionMap.FindAction("Crouch");
+        _interactAction = _playerActionMap.FindAction("Interact");
+
+        _pauseAction = _uiActionMap.FindAction("Pause");
     }
 
-    private void OnEnable() // Function to connect events after enabling
+    private void OnEnable()
     {
         _playerActionMap.Enable();
+        _uiActionMap.Enable();
 
-        _sprintAction.performed += ctx => sprintHeld = true;
-        _sprintAction.canceled += ctx => sprintHeld = false;
+        _sprintAction.performed += OnSprintPerformed;
+        _sprintAction.canceled += OnSprintCanceled;
 
-        _blinkAction.performed += ctx => blinkPressed = true;
+        _crouchAction.performed += OnCrouchPerformed;
+        _crouchAction.canceled += OnCrouchCanceled;
 
-        _crouchAction.performed += ctx => crouchHeld = true;
-        _crouchAction.canceled += ctx => crouchHeld = false;
+        _blinkAction.performed += OnBlinkPerformed;
+
+        _interactAction.performed += OnInteractPerformed;
+
+        _pauseAction.performed += OnPausePerformedHandler;
     }
 
-    private void OnDisable() // Function to disconnect events before disabling
+    private void OnDisable()
     {
-        _sprintAction.performed -= ctx => sprintHeld = true;
-        _sprintAction.canceled -= ctx => sprintHeld = false;
+        _sprintAction.performed -= OnSprintPerformed;
+        _sprintAction.canceled -= OnSprintCanceled;
 
-        _blinkAction.performed -= ctx => blinkPressed = true;
+        _crouchAction.performed -= OnCrouchPerformed;
+        _crouchAction.canceled -= OnCrouchCanceled;
 
-        _crouchAction.performed -= ctx => crouchHeld = true;
-        _crouchAction.canceled -= ctx => crouchHeld = false;
+        _blinkAction.performed -= OnBlinkPerformed;
+
+        _interactAction.performed -= OnInteractPerformed;
+
+        _pauseAction.performed -= OnPausePerformedHandler;
 
         _playerActionMap.Disable();
+        _uiActionMap.Disable();
     }
 
-    private void Update() // Constantly read the values of the player's controls
+    private void Update()
     {
         moveInput = _moveAction.ReadValue<Vector2>();
         lookInput = _lookAction.ReadValue<Vector2>();
     }
 
+    public void ResetCrouchPressed()
+    {
+        crouchPressed = false;
+    }
+
     public void ResetBlink()
     {
         blinkPressed = false;
+    }
+
+    public void EnableGameplayInputs() => _playerActionMap.Enable();
+    public void DisableGameplayInputs() => _playerActionMap.Disable();
+
+    public void EnableUIAssistedInputs() => _uiActionMap.Enable();
+    public void DisableUIAssistedInputs() => _uiActionMap.Disable();
+
+    private void OnSprintPerformed(InputAction.CallbackContext ctx) => sprintHeld = true;
+    private void OnSprintCanceled(InputAction.CallbackContext ctx) => sprintHeld = false;
+
+    private void OnCrouchPerformed(InputAction.CallbackContext ctx)
+    {
+        crouchHeld = true;
+        crouchPressed = true;
+    }
+    private void OnCrouchCanceled(InputAction.CallbackContext ctx) => crouchHeld = false;
+
+    private void OnBlinkPerformed(InputAction.CallbackContext ctx) => blinkPressed = true;
+
+    private void OnInteractPerformed(InputAction.CallbackContext ctx)
+    {
+        OnInteractPressed?.Invoke();
+    }
+
+    private void OnPausePerformedHandler(InputAction.CallbackContext ctx)
+    {
+        OnPauseInputPerformed?.Invoke();
     }
 }
