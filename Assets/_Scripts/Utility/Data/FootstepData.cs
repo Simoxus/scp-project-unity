0615b50dec983ceb99b7ccf451faaec8c1be22bc
Simoxus <simoxusmanagement@gmail.com>
2025-07-03 11:11:56 -0600

    Add ScriptableObject bases

---
+++ Assets/_Scripts/Utility/Data/FootstepData.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "FootstepData", menuName = "Custom Data/Player Footstep Data")]
+public class FootstepData : ScriptableObject
+{
+    public List<Texture> textures = new List<Texture>();
+    public string fmodParameterLabel;
+    public float fmodParameterValue;
+}
---
+++ Assets/_Scripts/Utility/Data/RoomData.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using EditorAttributes;
+using System.Collections.Generic;
+using System;
+//using Events.Base;
+
+namespace Rooms.Data
+{
+    [CreateAssetMenu(fileName = "RoomData", menuName = "Custom Data/Room ID Data")]
+    public class RoomData : ScriptableObject
+    {
+        [SerializeField, AssetPreview] public GameObject roomPrefab;
+
+        [Header("Room Info")]
+        public string roomID;
+        public string roomDisplayID;
+
+        [TextArea]
+        public string description;
+
+        [Header("Room Properties")]
+        public ZoneLocation zoneLocation;
+        public RoomType roomType;
+        public bool roomRequired;
+        [SerializeField, HideField(nameof(roomRequired))] public float roomRarity;
+
+        [Space]
+        public AllowedConnectDirections allowedConnectDirections;
+
+        [Header("Environment")]
+        //public bool featuresEvent;
+        //[SerializeField, IndentProperty(15f), ShowField(nameof(featuresEvent))] public RoomEvent featuredEvent;
+        public bool customAmbience;
+        [SerializeField, IndentProperty(15f), ShowField(nameof(customAmbience))] public AudioClip customAmbienceAudio;
+        public bool customFog;
+        [SerializeField, IndentProperty(15f), ShowField(nameof(customFog))] public Color customFogColor;
+    }
+
+    public enum ZoneLocation
+    {
+        SurfaceZone,
+        EntranceZone,
+        HeavyZone,
+        LightZone,
+        Custom
+    }
+
+    public enum RoomType
+    {
+        Containment,
+        Checkpoint,
+        DeadEnd,
+        Hallway,
+        Custom
+    }
+
+    [System.Serializable]
+    public struct AllowedConnectDirections
+    {
+        public bool north, east, south, west;
+    }
+}
