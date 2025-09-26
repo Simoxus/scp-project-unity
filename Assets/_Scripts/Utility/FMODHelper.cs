3a993cef3b652ce75390f57dd67aa9aad287d173
Simoxus <simoxusmanagement@gmail.com>
2025-09-26 16:52:30 -0600

    Add new helper functions to FMODHelper

--- Assets/_Scripts/Utility/FMODHelper.cs
+++ Assets/_Scripts/Utility/FMODHelper.cs
@@ -1,9 +1,11 @@
-using UnityEngine;
 using FMODUnity;
-using FMOD.Studio; // Important: Add this using statement
+using System.Collections.Generic;
+using UnityEngine;
 
 public static class FMODHelper
 {
+    private static readonly Dictionary<string, FMOD.Studio.EventInstance> activeInstances = new();
+
     public static void PlayOneShotWithParameters(EventReference fmodEvent, Vector3 position, params (string name, float value)[] parameters)
     {
         var instance = RuntimeManager.CreateInstance(fmodEvent);
@@ -21,5 +23,32 @@ public static class FMODHelper
     public static void PlayOneShot3D(EventReference fmodEvent, Vector3 position)
     {
         RuntimeManager.PlayOneShot(fmodEvent, position);
+    }
+
+    public static void PlayInstance(EventReference fmodEvent, string key, Vector3 position)
+    {
+        // Stop & release old instance if still running
+        if (activeInstances.TryGetValue(key, out var existing))
+        {
+            existing.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            existing.release();
+            activeInstances.Remove(key);
+        }
+
+        var instance = RuntimeManager.CreateInstance(fmodEvent);
+        instance.set3DAttributes(RuntimeUtils.To3DAttributes(position));
+        instance.start();
+
+        activeInstances[key] = instance;
+    }
+
+    public static void StopInstance(string key, bool allowFadeout = true)
+    {
+        if (activeInstances.TryGetValue(key, out var instance))
+        {
+            instance.stop(allowFadeout ? FMOD.Studio.STOP_MODE.ALLOWFADEOUT : FMOD.Studio.STOP_MODE.IMMEDIATE);
+            instance.release();
+            activeInstances.Remove(key);
+        }
     }
 }
