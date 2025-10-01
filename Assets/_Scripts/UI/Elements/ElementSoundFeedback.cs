841e0dd9182d12fd7c091e47f85bf2fd22113886
Simoxus <simoxusmanagement@gmail.com>
2025-09-30 21:41:02 -0600

    Add simple UI element class for sound feedback

---
+++ Assets/_Scripts/UI/Elements/ElementSoundFeedback.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using FMODUnity;
+
+public class ElementSoundFeedback : MonoBehaviour
+{
+    [Tooltip("The AudioSource component used to play the sound.")]
+    public EventReference uiPressEvent;
+
+    public void PlaySound()
+    {
+        FMODHelper.PlayOneShot(uiPressEvent);
+    }
+}
