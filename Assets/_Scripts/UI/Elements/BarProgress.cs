ca3f1f7dd7fd2fac8c153fd987c3a95acefa85e0
Simoxus <simoxusmanagement@gmail.com>
2025-09-30 21:37:04 -0600

    Add get functions to BarProgress UI element helper

--- Assets/_Scripts/UI/Elements/BarProgress.cs
+++ Assets/_Scripts/UI/Elements/BarProgress.cs
@@ -6,19 +6,31 @@ public class BarProgress : MonoBehaviour
 {
     private GameObject[] bars; // array for the bars
 
-    // Cache bars in the GameObject's children (ayo?)
-    // note from future me: it was like 2 AM when i wrote the comment above
     void Awake()
     {
         // Get all children and store them in an array
         bars = new GameObject[transform.childCount];
+
         for (int i = 0; i < transform.childCount; i++)
         {
             bars[i] = transform.GetChild(i).gameObject;
         }
     }
 
-    // Set progress as fraction, from 0 to 1
+    // Get progress from 0 to 1
+    public float GetProgress()
+    {
+        int activeBars = bars.Count(b => b.activeSelf);
+        return (float)activeBars / bars.Length;
+    }
+
+    // Get progress by bar count
+    public int GetProgressBars()
+    {
+        return bars.Count(b => b.activeSelf);
+    }
+
+    // Set progress from 0 to 1
     public void SetProgress(float progress)
     {
         progress = Mathf.Clamp01(progress);
@@ -26,7 +38,7 @@ public class BarProgress : MonoBehaviour
         UpdateBars(activeBars);
     }
 
-    // Set progress directly, by bar count
+    // Set progress by bar count
     public void SetProgressBars(int count)
     {
         count = Mathf.Clamp(count, 0, bars.Length);
@@ -35,7 +47,6 @@ public class BarProgress : MonoBehaviour
 
     private void UpdateBars(int activeCount)
     {
-        // Deactivate bars that are no longer needed starting from the last active bar
         for (int i = bars.Length - 1; i >= activeCount; i--)
         {
             if (bars[i].activeSelf)
@@ -44,7 +55,6 @@ public class BarProgress : MonoBehaviour
             }
         }
 
-        // Activate bars that are now needed up to the new active count
         for (int i = 0; i < activeCount; i++)
         {
             if (!bars[i].activeSelf)
