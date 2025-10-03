8bf667b5a189c38fdff98ac604c331f377c2ba3e
Simoxus <simoxusmanagement@gmail.com>
2025-10-02 21:58:46 -0600

    Make ClearPrefs command also clear EditorPrefs

--- Assets/_Scripts/Systems/DebugConsole/Commands/ClearPrefsCommand.cs
+++ Assets/_Scripts/Systems/DebugConsole/Commands/ClearPrefsCommand.cs
@@ -1,3 +1,4 @@
+using UnityEditor;
 using UnityEngine;
 
 namespace Console.Commands
@@ -5,13 +6,24 @@ namespace Console.Commands
     public class ClearPrefsCommand : ConsoleBase
     {
         public override string CommandWord => "clearprefs";
+
+#if UNITY_EDITOR
+        public override string Description => "Deletes all PlayerPrefs and EditorPrefs (EDITOR ONLY) data.";
+#else
         public override string Description => "Deletes all PlayerPrefs data.";
+#endif
+
         protected override string RawUsage => "clearprefs";
 
         public override void Execute(string[] args)
         {
             PlayerPrefs.DeleteAll();
             ConsoleManager.LogToConsole($"<color=#33CC33>All PlayerPrefs data has been cleared.</color>");
+
+#if UNITY_EDITOR
+            EditorPrefs.DeleteAll();
+            ConsoleManager.LogToConsole("<color=#33CC33>All EditorPrefs data has been cleared.</color>");
+#endif
         }
     }
 }
