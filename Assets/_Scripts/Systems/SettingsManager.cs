dbc2be7b559f0f0fdb73cb5f575a5c4cfa2f17e0
Simoxus <simoxusmanagement@gmail.com>
2025-10-02 21:59:30 -0600

    Make SettingsManager manage PlayerPrefs keys

--- Assets/_Scripts/Systems/SettingsManager.cs
+++ Assets/_Scripts/Systems/SettingsManager.cs
@@ -15,5 +15,40 @@ public class SettingsManager : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
+    public void SaveInt(string category, string key, int value)
+    {
+        PlayerPrefs.SetInt($"{category}.{key}", value);
+    }
+
+    public int LoadInt(string category, string key, int defaultValue = 0)
+    {
+        return PlayerPrefs.GetInt($"{category}.{key}", defaultValue);
+    }
+
+    public void SaveFloat(string category, string key, float value)
+    {
+        PlayerPrefs.SetFloat($"{category}.{key}", value);
+    }
+
+    public float LoadFloat(string category, string key, float defaultValue = 0f)
+    {
+        return PlayerPrefs.GetFloat($"{category}.{key}", defaultValue);
+    }
+
+    public void SaveBool(string category, string key, bool value)
+    {
+        PlayerPrefs.SetInt($"{category}.{key}", value ? 1 : 0);
+    }
+
+    public bool LoadBool(string category, string key, bool defaultValue = false)
+    {
+        return PlayerPrefs.GetInt($"{category}.{key}", defaultValue ? 1 : 0) == 1;
+    }
+
+    public void Save()
+    {
+        PlayerPrefs.Save();
+    }
 }
 
