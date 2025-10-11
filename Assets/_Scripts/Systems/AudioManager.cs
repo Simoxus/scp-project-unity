bb967a3c51b48100bc34f7b5606df89f93de1fc6
Simoxus <simoxusmanagement@gmail.com>
2025-10-10 18:52:56 -0600

    Refactor AudioManager, transition over to VCAs

--- Assets/_Scripts/Systems/AudioManager.cs
+++ Assets/_Scripts/Systems/AudioManager.cs
@@ -1,3 +1,4 @@
+using Cysharp.Threading.Tasks;
 using FMOD.Studio;
 using FMODUnity;
 using UnityEngine;
@@ -7,25 +8,46 @@ public class AudioManager : MonoBehaviour
     public static AudioManager Instance { get; private set; }
 
     private Bus _gameplayBus;
-    private Bus _uiBus;
+    private Bus _persistentBus;
+
+    private VCA _masterVCA;
+    private VCA _sfxVCA;
+    private VCA _musicVCA;
+    private VCA _voVCA;
+
+    public void SetMasterVolume(float volume) => _masterVCA.setVolume(Mathf.Clamp01(volume));
+    public void SetSFXVolume(float volume) => _sfxVCA.setVolume(Mathf.Clamp01(volume));
+    public void SetMusicVolume(float volume) => _musicVCA.setVolume(Mathf.Clamp01(volume));
+    public void SetVOVolume(float volume) => _voVCA.setVolume(Mathf.Clamp01(volume));
+
+    public float GetMasterVolume() { _masterVCA.getVolume(out float v); return v; }
+    public float GetSFXVolume()    { _sfxVCA.getVolume(out float v);    return v; }
+    public float GetMusicVolume()  { _musicVCA.getVolume(out float v);  return v; }
+    public float GetVOVolume() { _voVCA.getVolume(out float v); return v; }
 
     private void Awake()
     {
-        if (Instance == null) Instance = this;
-        else Destroy(gameObject);
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
 
         _gameplayBus = RuntimeManager.GetBus("bus:/Gameplay");
-        _uiBus = RuntimeManager.GetBus("bus:/UI");
+        _persistentBus = RuntimeManager.GetBus("bus:/Persistent");
+
+        _masterVCA  = RuntimeManager.GetVCA("vca:/Master");
+        _sfxVCA = RuntimeManager.GetVCA("vca:/SFX");
+        _musicVCA = RuntimeManager.GetVCA("vca:/Music");
+        _voVCA = RuntimeManager.GetVCA("vca:/VO");
     }
 
-    public void ToggleSounds(bool doPause)
+    public void ToggleGameSounds(bool doPause)
     {
         _gameplayBus.setPaused(doPause);
-    }
-
-    public void SetBusVolume(float volume, string busName)
-    {
-        Bus requestedBus = RuntimeManager.GetBus(busName);
-        requestedBus.setVolume(volume);
     }
 }
