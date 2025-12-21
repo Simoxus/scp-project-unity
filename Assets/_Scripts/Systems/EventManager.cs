42feba0f88dc0253aba5322e7b4ff977bb4ede1e
Simoxus <simoxusmanagement@gmail.com>
2025-12-21 16:56:07 -0700

    Placeholder for EventManager

--- Assets/_Scripts/Systems/EventManager.cs
+++ Assets/_Scripts/Systems/EventManager.cs
@@ -2,6 +2,20 @@ using UnityEngine;
 
 public class EventManager : MonoBehaviour
 {
+    public static EventManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Log.VerboseWarning($"Duplicate instance of {GetType().Name} found. Destroying the new one.");
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
