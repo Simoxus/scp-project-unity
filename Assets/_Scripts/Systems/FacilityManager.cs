dda78eae63f94babd8f4dc018285833033787d3a
Simoxus <simoxusmanagement@gmail.com>
2025-12-21 16:56:17 -0700

    Placeholder for FacilityManager

--- Assets/_Scripts/Systems/FacilityManager.cs
+++ Assets/_Scripts/Systems/FacilityManager.cs
@@ -2,5 +2,17 @@
 
 public class FacilityManager : MonoBehaviour
 {
+    public static FacilityManager Instance { get; private set; }
 
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Log.VerboseWarning($"Duplicate instance of {GetType().Name} found. Destroying the new one.");
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+    }
 }
