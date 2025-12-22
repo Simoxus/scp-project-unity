967b9edad769c37adeca6f954cbd3f9f772909b9
Simoxus <simoxusmanagement@gmail.com>
2025-12-21 17:26:49 -0700

    Add cell door

---
+++ Assets/_Scripts/Scriptables/Door/Cell/CellDoorController.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class CellDoorController : BaseDoorController
+{
+    protected override void OnSetActivatorsState(bool enabled)
+    {
+    }
+
+    protected override void OnApplyLockedVisuals(Color color, string message)
+    {
+    }
+
+    protected override void OnApplyBrokenVisuals(Color color)
+    {
+    }
+
+    protected override void OnResetActivatorVisuals(Color color)
+    {
+    }
+
+    protected override void OnStopActivatorsPulse()
+    {
+    }
+
+    protected override void OnStartActivatorsPulse(Color color, float? customDuration = null, float? customIntensity = null)
+    {
+    }
+
+    protected override void OnTransitionToPulse(Color targetColor, float transitionDuration)
+    {
+    }
+}
