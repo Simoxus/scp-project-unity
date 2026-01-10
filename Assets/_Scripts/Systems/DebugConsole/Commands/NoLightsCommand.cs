3a89e1683861e5b0042b4d1ebacf92a0001e6a09
Simoxus <simoxusmanagement@gmail.com>
2026-01-09 22:08:32 -0700

    Add nolights command

---
+++ Assets/_Scripts/Systems/DebugConsole/Commands/NoLightsCommand.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering.Universal;
+namespace Console.Commands
+{
+    public class NoLightsCommand : BaseConsole
+    {
+        public override string CommandWord => "nolights";
+        public override string Description => "Disables all lighting.";
+        protected override string RawUsage => "nolights";
+
+        private static bool isEnabled = false;
+
+        static NoLightsCommand()
+        {
+            Application.quitting += ResetOnQuit;
+        }
+
+        private static void ResetOnQuit()
+        {
+            UniversalRenderPipelineDebugDisplaySettings debugDisplaySettings = UniversalRenderPipelineDebugDisplaySettings.Instance;
+            debugDisplaySettings.lightingSettings.lightingDebugMode = DebugLightingMode.None;
+        }
+
+        public override void Execute(string[] args)
+        {
+            UniversalRenderPipelineDebugDisplaySettings debugDisplaySettings = UniversalRenderPipelineDebugDisplaySettings.Instance;
+
+            isEnabled = !isEnabled;
+
+            debugDisplaySettings.lightingSettings.lightingDebugMode = isEnabled
+                ? DebugLightingMode.Reflections
+                : DebugLightingMode.None;
+
+            ConsoleManager.LogToConsole($"Lighting has been {(isEnabled ? "disabled" : "enabled")}.".AsSuccess());
+        }
+    }
+}
