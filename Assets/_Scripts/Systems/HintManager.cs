ace0193acdde3e395ae11cfa3819bd1b8fd8135c
Simoxus <simoxusmanagement@gmail.com>
2026-01-09 21:30:08 -0700

    Add skeleton for HintManager

---
+++ Assets/_Scripts/Systems/HintManager.cs
@@ -0,0 +1,24 @@
+﻿[System.Serializable]
+public struct LocalizationString
+{
+    public string Table;
+    public string Key;
+
+    public LocalizationString(string table, string key)
+    {
+        Table = table;
+        Key = key;
+    }
+
+    public string GetLocalized(params object[] args)
+    {
+        return LocalizationHelper.GetString(Table, Key, args);
+    }
+
+    public bool IsEmpty => string.IsNullOrEmpty(Table) || string.IsNullOrEmpty(Key);
+}
+
+public class HintManager : Singleton<HintManager>
+{
+
+}
