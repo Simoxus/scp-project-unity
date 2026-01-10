83ed69d3169fe9ac4b43609d91ba044f9289edba
Simoxus <simoxusmanagement@gmail.com>
2026-01-09 22:08:45 -0700

    Add noclip command

---
+++ Assets/_Scripts/Systems/DebugConsole/Commands/NoclipCommand.cs
@@ -0,0 +1,26 @@
+﻿namespace Console.Commands
+{
+    public class NoclipCommand : BaseConsole
+    {
+        public override string CommandWord => "noclip";
+        public override string Description => "Toggles noclip mode (fly through walls).";
+        protected override string RawUsage => "noclip";
+
+        public override void Execute(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                ConsoleManager.LogToConsole(Usage.AsError());
+                return;
+            }
+
+            Player player = Core.Player;
+            if (player == null) return;
+
+            player.PlayerController.ToggleNoclip();
+
+            string status = player.IsInState(PlayerState.Noclip) ? "enabled" : "disabled";
+            ConsoleManager.LogToConsole($"Noclip has been {status}.".AsSuccess());
+        }
+    }
+}
