0d4e14b6628d98a7aa643c2db9fe26087387d2b8
Simoxus <simoxusmanagement@gmail.com>
2026-01-09 21:28:09 -0700

    Refactor FacilityManager to Singleton

--- Assets/_Scripts/Systems/FacilityManager.cs
+++ Assets/_Scripts/Systems/FacilityManager.cs
@@ -1,18 +1,4 @@
-﻿using UnityEngine;
-
-public class FacilityManager : MonoBehaviour
+﻿public class FacilityManager : Singleton<FacilityManager>
 {
-    public static FacilityManager Instance { get; private set; }
 
-    private void Awake()
-    {
-        if (Instance != null && Instance != this)
-        {
-            Log.VerboseWarning($"Duplicate instance of {GetType().Name} found. Destroying the new one.");
-            Destroy(gameObject);
-            return;
-        }
-
-        Instance = this;
-    }
 }
