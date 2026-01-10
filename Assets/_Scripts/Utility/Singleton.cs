b57726250701c64132efdb836a46f77242cba096
Simoxus <simoxusmanagement@gmail.com>
2026-01-09 21:21:54 -0700

    Add singleton base class

---
+++ Assets/_Scripts/Utility/Singleton.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+    private static readonly object _lock = new object();
+    private static bool _isQuitting = false;
+
+    public static T Instance
+    {
+        get
+        {
+            if (_isQuitting)
+            {
+                return null;
+            }
+
+            lock (_lock)
+            {
+                if (_instance == null)
+                {
+                    _instance = FindAnyObjectByType<T>();
+
+                    if (_instance == null)
+                    {
+                        Log.VerboseWarning($"No instance of {typeof(T)} found in scene.");
+                    }
+                }
+
+                return _instance;
+            }
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Log.VerboseWarning($"Duplicate instance of {GetType().Name} found. Destroying the new one.");
+            Destroy(gameObject);
+            return;
+        }
+
+        _instance = this as T;
+        OnAwake();
+    }
+
+    protected virtual void OnDestroy()
+    {
+        if (_instance == this)
+        {
+            _isQuitting = true;
+            _instance = null;
+        }
+    }
+
+    protected virtual void OnAwake() { }
+}
