1963d779bef0051c89c4edd841dd51def886a909
Simoxus <simoxusmanagement@gmail.com>
2026-01-09 21:27:14 -0700

    Refactor CameraManager to Singleton

--- Assets/_Scripts/Systems/CameraManager.cs
+++ Assets/_Scripts/Systems/CameraManager.cs
@@ -1,52 +1,21 @@
-using Unity.Cinemachine;
 using UnityEngine;
 
-public class CameraManager : MonoBehaviour
+public class CameraManager : Singleton<CameraManager>
 {
-    public static CameraManager Instance { get; private set; }
-
-    public CinemachineBrain cameraBrain;
-    public CinemachineCamera cameraMain;
-    public CinemachineImpulseSource impulseSource;
-
-    private void Reset()
-    {
-        cameraBrain = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CinemachineBrain>();
-        cameraMain = GameObject.Find("CameraMain").GetComponent<CinemachineCamera>();
-        impulseSource = GameObject.FindGameObjectWithTag("ImpulseSource").GetComponent<CinemachineImpulseSource>();
-    }
-
-    private void Awake()
-    {
-        if (Instance != null && Instance != this)
-        {
-            Log.VerboseWarning($"Duplicate instance of {GetType().Name} found. Destroying the new one.");
-            Destroy(gameObject);
-            return;
-        }
-
-        Instance = this;
-
-        if (impulseSource == null)
-        {
-            Log.VerboseWarning("CameraManager is missing reference to a Cinemachine Impulse Source. Please assign it.");
-        }
-    }
-
     public void GenerateShake(float intensity)
     {
-        if (impulseSource == null) return;
-        if (GameManager.Instance.cameraShaking == false) return;
+        if (Core.Player.CameraImpulseSource == null) return;
+        if (Core.GameManager.cameraShaking == false) return;
 
         Vector3 impulseVelocity = new Vector3(intensity, intensity, intensity);
-        impulseSource.GenerateImpulseWithVelocity(impulseVelocity);
+        Core.Player.CameraImpulseSource.GenerateImpulseWithVelocity(impulseVelocity);
     }
 
     public void GenerateShakeWithVector3(Vector3 impulseVelocity)
     {
-        if (impulseSource == null) return;
-        if (GameManager.Instance.cameraShaking == false) return;
+        if (Core.Player.CameraImpulseSource == null) return;
+        if (Core.GameManager.cameraShaking == false) return;
 
-        impulseSource.GenerateImpulseWithVelocity(impulseVelocity);
+        Core.Player.CameraImpulseSource.GenerateImpulseWithVelocity(impulseVelocity);
     }
 }
