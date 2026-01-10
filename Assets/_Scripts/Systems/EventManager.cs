2b5cb81e8c74d265e0c08939385687f63d376020
Simoxus <simoxusmanagement@gmail.com>
2026-01-09 21:27:58 -0700

    Refactor EventManager to Singleton

--- Assets/_Scripts/Systems/EventManager.cs
+++ Assets/_Scripts/Systems/EventManager.cs
@@ -1,30 +1,14 @@
-using UnityEngine;
-
-public class EventManager : MonoBehaviour
+public class EventManager : Singleton<EventManager>
 {
-    public static EventManager Instance { get; private set; }
-
-    private void Awake()
-    {
-        if (Instance != null && Instance != this)
-        {
-            Log.VerboseWarning($"Duplicate instance of {GetType().Name} found. Destroying the new one.");
-            Destroy(gameObject);
-            return;
-        }
-
-        Instance = this;
-    }
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
