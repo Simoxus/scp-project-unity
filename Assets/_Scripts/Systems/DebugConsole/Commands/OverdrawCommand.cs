b745d7e6b13a695eddc34e4de6e0e4ad2a60d009
Simoxus <simoxusmanagement@gmail.com>
2026-01-09 22:08:40 -0700

    Add overdraw command

---
+++ Assets/_Scripts/Systems/DebugConsole/Commands/OverdrawCommand.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+namespace Console.Commands
+{
+    public class OverdrawCommand : BaseConsole
+    {
+        public override string CommandWord => "overdraw";
+        public override string Description => "Toggles overdraw visualization.";
+        protected override string RawUsage => "overdraw";
+
+        private static bool isOverdraw = false;
+
+        static OverdrawCommand()
+        {
+            Application.quitting += ResetOnQuit;
+        }
+
+        private static void ResetOnQuit()
+        {
+            UniversalRenderPipelineDebugDisplaySettings debugDisplaySettings = UniversalRenderPipelineDebugDisplaySettings.Instance;
+            debugDisplaySettings.renderingSettings.overdrawMode = DebugOverdrawMode.None;
+        }
+
+        public override void Execute(string[] args)
+        {
+            UniversalRenderPipelineDebugDisplaySettings debugDisplaySettings = UniversalRenderPipelineDebugDisplaySettings.Instance;
+
+            isOverdraw = !isOverdraw;
+
+            if (isOverdraw)
+            {
+                debugDisplaySettings.renderingSettings.overdrawMode = DebugOverdrawMode.All;
+                ConsoleManager.LogToConsole("Overdraw has been enabled.".AsSuccess());
+            }
+            else
+            {
+                debugDisplaySettings.renderingSettings.overdrawMode = DebugOverdrawMode.None;
+                ConsoleManager.LogToConsole("Overdraw has been disabled.".AsSuccess());
+            }
+        }
+    }
+}
