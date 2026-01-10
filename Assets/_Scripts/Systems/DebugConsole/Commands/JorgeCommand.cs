f0a3f1c0ea12637b979776cc840c0b497a838147
Simoxus <simoxusmanagement@gmail.com>
2026-01-09 22:07:55 -0700

    Add Jorge command

---
+++ Assets/_Scripts/Systems/DebugConsole/Commands/JorgeCommand.cs
@@ -0,0 +1,15 @@
+﻿namespace Console.Commands
+{
+    public class JorgeCommand : BaseConsole
+    {
+        public override string CommandWord => "jorge";
+        public override string Description => "jorgejorgejorgejorgejorgejorgefcvenyfcvenyfcvenyfcvenyfcvenyfcveny";
+        protected override string RawUsage => "jorge";
+
+        public override void Execute(string[] args)
+        {
+            FMODHelper.PlayOneShot(Core.AudioDataAccess.Special.FcvenySound);
+            ConsoleManager.LogToConsole("JORGE HAS BEEN EXPECTING YOU".AsJorge());
+        }
+    }
+}
