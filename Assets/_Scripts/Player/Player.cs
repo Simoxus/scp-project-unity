f869f3d51caaa68fff6027a75793b5bd151c7b19
Simoxus <simoxusmanagement@gmail.com>
2026-01-09 22:32:40 -0700

    Use PascalCase for references, add quick checkers

--- Assets/_Scripts/Player/Player.cs
+++ Assets/_Scripts/Player/Player.cs
@@ -1,77 +1,82 @@
 using EditorAttributes;
+using FMODUnity;
 using Unity.Cinemachine;
 using UnityEngine;
 
 public enum PlayerState
 {
+    Freefall,
+    Noclip,
     Idle,
     Walking,
     Sprinting,
-    Crouching,
-    Freefall
+    Crouching
 }
 
-public class Player : MonoBehaviour
+public class Player : Singleton<Player>
 {
-    public static Player Instance { get; private set; }
-
-    [ReadOnly] public PlayerState currentState = PlayerState.Idle;
+    [ReadOnly] public PlayerState CurrentState = PlayerState.Idle;
 
     [Header("Scripts")]
-    public PlayerController playerController;
-    public PlayerInputs playerInputs;
-    public PlayerStats playerStats;
-    public PlayerHealth playerHealth;
-    public PlayerEffects playerEffects;
-    public PlayerBobbing playerBobbing;
-    public PlayerSanity playerSanity;
-    public PlayerFootsteps playerFootsteps;
-    public PlayerInteract playerInteract;
-    public PlayerFreecam playerFreecam;
+    public PlayerController PlayerController;
+    public PlayerInputs PlayerInputs;
+    public PlayerStats PlayerStats;
+    public PlayerHealth PlayerHealth;
+    public PlayerBobbing PlayerBobbing;
+    public PlayerSanity PlayerSanity;
+    public PlayerFootsteps PlayerFootsteps;
+    public PlayerInteract PlayerInteract;
+    public PlayerFreecam PlayerFreecam;
 
     [Header("Components")]
-    public CharacterController characterController;
-    public GameObject cameraRoot;
-    public Camera cameraBrain;
-    public CinemachineCamera cameraMain;
-    public CinemachineImpulseListener cameraImpulseListener;
-    public CinemachineImpulseSource cameraImpulseSource;
+    public CharacterController CharacterController;
+    public GameObject CameraRoot;
+    public Camera CameraBrain;
+    public CinemachineCamera CameraMain;
+    public CinemachineBrain CameraSettings;
+    public CinemachineImpulseListener CameraImpulseListener;
+    public CinemachineImpulseSource CameraImpulseSource;
+    public StudioListener CameraBrainStudioListener;
 
-    private void Awake()
+    public void Reset()
     {
-        if (Instance != null && Instance != this)
-        {
-            Destroy(this.gameObject);
-        }
-        else
-        {
-            Instance = this;
-        }
+        AssignReferences();
     }
 
-    private void Reset()
+    public bool CanMove()
     {
-        AutoAssignReferences();
+        return CurrentState != PlayerState.Noclip
+            && PlayerController != null
+            && PlayerController.enabled;
     }
 
-    private void AutoAssignReferences()
-    {
-        playerController = GetComponent<PlayerController>();
-        playerInputs = GetComponent<PlayerInputs>();
-        playerStats = GetComponent<PlayerStats>();
-        playerHealth = GetComponent<PlayerHealth>();
-        playerEffects = GetComponent<PlayerEffects>();
-        playerBobbing = GetComponent<PlayerBobbing>();
-        playerSanity = GetComponent<PlayerSanity>();
-        playerFootsteps = GetComponent<PlayerFootsteps>();
-        playerInteract = GetComponent<PlayerInteract>();
-        playerFreecam = GetComponent<PlayerFreecam>();
+    public bool IsInState(PlayerState state) => CurrentState == state;
 
-        characterController = GetComponent<CharacterController>();
-        cameraBrain = GetComponentInChildren<Camera>();
-        cameraMain = GetComponentInChildren<CinemachineCamera>();
-        cameraRoot = cameraBrain.transform.parent.gameObject;
-        cameraImpulseListener = GetComponentInChildren<CinemachineImpulseListener>();
-        cameraImpulseSource = GetComponentInChildren<CinemachineImpulseSource>();
+    public bool IsMoving() => PlayerController != null && PlayerController.IsMoving;
+
+    public bool IsGrounded() => CharacterController != null && CharacterController.isGrounded;
+
+    private void AssignReferences()
+    {
+        // Assign scripts
+        PlayerController = GetComponent<PlayerController>();
+        PlayerInputs = GetComponent<PlayerInputs>();
+        PlayerStats = GetComponent<PlayerStats>();
+        PlayerHealth = GetComponent<PlayerHealth>();
+        PlayerBobbing = GetComponent<PlayerBobbing>();
+        PlayerSanity = GetComponent<PlayerSanity>();
+        PlayerFootsteps = GetComponent<PlayerFootsteps>();
+        PlayerInteract = GetComponent<PlayerInteract>();
+        PlayerFreecam = GetComponent<PlayerFreecam>();
+
+        // Assign components
+        CharacterController = GetComponent<CharacterController>();
+        CameraRoot = GetComponentInChildren<CinemachineCamera>().transform.parent.gameObject;
+        CameraBrain = GetComponentInChildren<Camera>();
+        CameraMain = GetComponentInChildren<CinemachineCamera>();
+        CameraSettings = GetComponentInChildren<CinemachineBrain>();
+        CameraImpulseListener = GetComponentInChildren<CinemachineImpulseListener>();
+        CameraImpulseSource = GetComponentInChildren<CinemachineImpulseSource>();
+        CameraBrainStudioListener = GetComponentInChildren<StudioListener>();
     }
 }
