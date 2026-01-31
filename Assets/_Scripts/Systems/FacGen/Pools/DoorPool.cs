e9613033f97d649b2aa864568542bf1c7caffb29
Simoxus <simoxusmanagement@gmail.com>
2026-01-31 14:05:14 -0700

    Add FG pool for doors

---
+++ Assets/_Scripts/Systems/FacGen/Pools/DoorPool.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace Facility.Generation
+{
+    [CreateAssetMenu(fileName = "_DoorPool_", menuName = "Custom/Map Gen/Door Pool")]
+    public class DoorPool : ScriptableObject
+    {
+        [SerializeField] private List<AssetReferenceGameObject> doorPrefabReferences = new List<AssetReferenceGameObject>();
+
+        public AssetReferenceGameObject GetRandomDoorReference(int seed)
+        {
+            if (doorPrefabReferences.Count == 0)
+            {
+                return null;
+            }
+
+            Random.InitState(seed);
+            return doorPrefabReferences[Random.Range(0, doorPrefabReferences.Count)];
+        }
+
+        public int GetDoorCount() => doorPrefabReferences.Count;
+    }
+}
