65eac6ef9031ad04effa36340554c33a2ad7ed70
Simoxus <simoxusmanagement@gmail.com>
2026-01-31 12:32:12 -0700

    Stop using Reflection for door commands and simplify

--- Assets/_Scripts/Systems/DebugConsole/Commands/DoorAllCommand.cs
+++ Assets/_Scripts/Systems/DebugConsole/Commands/DoorAllCommand.cs
@@ -1,6 +1,4 @@
 ﻿using Cysharp.Threading.Tasks;
-using System.Linq;
-using System.Reflection;
 using UnityEngine;
 
 namespace Console.Commands
@@ -8,8 +6,8 @@ namespace Console.Commands
     public class DoorAllCommand : BaseConsole
     {
         public override string CommandWord => "doorall";
-        public override string Description => "Toggles or breaks all doors and gates.";
-        protected override string RawUsage => "doorall <toggle|break>";
+        public override string Description => "Opens, closes, toggles, or breaks all doors.";
+        protected override string RawUsage => "doorall <open|close|toggle|break>";
 
         public override async void Execute(string[] args)
         {
@@ -20,111 +18,65 @@ namespace Console.Commands
             }
 
             string action = args[0].ToLower();
-
-            if (action != "toggle" && action != "break")
+            if (action != "open" && action != "close" && action != "toggle" && action != "break")
             {
-                ConsoleManager.LogToConsole("Invalid argument. Use 'toggle' or 'break'.".AsError());
+                ConsoleManager.LogToConsole(Usage.AsError());
                 return;
             }
 
             int doorCount = 0;
-            int gateCount = 0;
-
             var doorTasks = new System.Collections.Generic.List<UniTask>();
-            var gateTasks = new System.Collections.Generic.List<UniTask>();
 
             // Find all doors
             GameObject[] doors = GameObject.FindGameObjectsWithTag("Door");
             foreach (GameObject door in doors)
             {
-                var activator = door.GetComponentInChildren<BaseDoorActivator>();
-                if (activator != null)
-                {
-                    var controllerField = activator.GetType().GetField("targetDoorController");
-                    if (controllerField != null)
-                    {
-                        var controller = controllerField.GetValue(activator);
-                        if (controller != null)
-                        {
-                            var controllerType = controller.GetType();
-                            MethodInfo method = null;
-
-                            if (action == "toggle")
-                                method = controllerType.GetMethod("ToggleDoor");
-                            else
-                                method = controllerType.GetMethod("BreakDoor");
-
-                            if (method != null)
-                            {
-                                doorTasks.Add((UniTask)method.Invoke(controller, null));
-                                doorCount++;
-                            }
-                        }
-                    }
-                }
-            }
-
-            // Find all gates
-            GameObject[] gates = GameObject.FindGameObjectsWithTag("Gate");
-            foreach (GameObject gate in gates)
-            {
-                var activator = gate.GetComponentInChildren<BaseGateActivator>();
-                if (activator != null)
-                {
-                    var controllerField = activator.GetType().GetField("targetGateController");
-                    if (controllerField != null)
-                    {
-                        var controller = controllerField.GetValue(activator);
-                        if (controller != null)
-                        {
-                            var controllerType = controller.GetType();
-                            MethodInfo method = null;
-
-                            if (action == "toggle")
-                                method = controllerType.GetMethod("ToggleGate");
-                            else
-                                method = controllerType.GetMethod("BreakGate");
-
-                            if (method != null)
-                            {
-                                gateTasks.Add((UniTask)method.Invoke(controller, null));
-                                gateCount++;
-                            }
-                        }
-                    }
-                }
-            }
-
-            // Find all cell doors (doors without activators)
-            foreach (GameObject door in doors)
-            {
-                var controller = door.GetComponent<BaseDoorController>();
+                var controller = GetDoorController(door);
                 if (controller != null)
                 {
-                    var hasActivator = door.GetComponentInChildren<BaseDoorActivator>() != null;
-                    if (!hasActivator)
+                    switch (action)
                     {
-                        var controllerType = controller.GetType();
-                        MethodInfo method = null;
-
-                        if (action == "toggle")
-                            method = controllerType.GetMethod("ToggleDoor");
-                        else
-                            method = controllerType.GetMethod("BreakDoor");
-
-                        if (method != null)
-                        {
-                            doorTasks.Add((UniTask)method.Invoke(controller, null));
+                        case "open":
+                            controller.OpenDoor();
                             doorCount++;
-                        }
+                            break;
+
+                        case "close":
+                            controller.CloseDoor();
+                            doorCount++;
+                            break;
+
+                        case "toggle":
+                            doorTasks.Add(controller.ToggleDoor());
+                            doorCount++;
+                            break;
+
+                        case "break":
+                            doorTasks.Add(controller.BreakDoor());
+                            doorCount++;
+                            break;
                     }
                 }
             }
 
-            await UniTask.WhenAll(doorTasks.Concat(gateTasks));
+            // Wait for all async operations to complete
+            if (doorTasks.Count > 0)
+            {
+                await UniTask.WhenAll(doorTasks);
+            }
 
-            string actionText = action == "toggle" ? "toggled" : "broken";
-            ConsoleManager.LogToConsole($"{doorCount} doors {actionText}, {gateCount} gates toggled.".AsSuccess());
+            string actionText = action switch
+            {
+                "open" => "opened",
+                "close" => "closed",
+                "toggle" => "toggled",
+                "break" => "broken",
+                _ => action
+            };
+
+            ConsoleManager.LogToConsole($"{doorCount} doors {actionText}.".AsSuccess());
         }
+
+        private BaseDoorController GetDoorController(GameObject door) => door.GetComponent<BaseDoorController>();
     }
 }
--- Assets/_Scripts/Systems/DebugConsole/Commands/DoorCommand.cs
+++ Assets/_Scripts/Systems/DebugConsole/Commands/DoorCommand.cs
@@ -1,4 +1,3 @@
-using Cysharp.Threading.Tasks;
 using FMODUnity;
 using Unity.Cinemachine;
 using UnityEngine;
@@ -8,8 +7,8 @@ namespace Console.Commands
     public class DoorCommand : BaseConsole
     {
         public override string CommandWord => "door";
-        public override string Description => "Casts a ray from the player's camera to toggle or break a door.";
-        protected override string RawUsage => "door <toggle|break>";
+        public override string Description => "Casts a ray from the camera to open, close, toggle, or break a door.";
+        protected override string RawUsage => "door <open|close|toggle|break>";
 
         public override async void Execute(string[] args)
         {
@@ -22,20 +21,21 @@ namespace Console.Commands
             string action = args[0].ToLower();
 
             CinemachineCamera cameraMain = Core.Player.CameraMain;
-            if (cameraMain == null) return;
-
-            Ray ray = new(cameraMain.transform.position, cameraMain.transform.forward);
-            if (!Physics.Raycast(ray, out RaycastHit hit, 15f)) return;
-
-            MonoBehaviour activator = FindActivator(hit.collider.gameObject);
-            BaseDoorController doorController = null;
-
-            if (activator == null)
+            if (cameraMain == null)
             {
-                doorController = FindDoorController(hit.collider.gameObject);
+                return;
             }
 
-            if (activator == null && doorController == null)
+            Ray ray = new(cameraMain.transform.position, cameraMain.transform.forward);
+            if (!Physics.Raycast(ray, out RaycastHit hit, 15f))
+            {
+                return;
+            }
+
+            BaseDoorController doorController = FindDoorController(hit.collider.gameObject);
+
+            // If not found on hit object, try grandparent (for doors with physics colliders)
+            if (doorController == null)
             {
                 StudioEventEmitter fmodCollision = hit.collider.GetComponent<StudioEventEmitter>();
                 Rigidbody rb = hit.collider.GetComponent<Rigidbody>();
@@ -45,168 +45,74 @@ namespace Console.Commands
                     Transform grandparent = hit.collider.transform.parent?.parent;
                     if (grandparent != null)
                     {
-                        activator = FindActivator(grandparent.gameObject);
-
-                        if (activator == null)
-                        {
-                            doorController = FindDoorController(grandparent.gameObject);
-                        }
+                        doorController = FindDoorController(grandparent.gameObject);
                     }
                 }
             }
 
-            if (activator == null && doorController == null)
+            if (doorController == null)
             {
-                ConsoleManager.LogToConsole("The object hit is not a valid door activator.".AsError());
+                ConsoleManager.LogToConsole("The object hit is not a valid door.".AsError());
                 return;
             }
 
+            string doorTypeName = GetDoorTypeName(hit.collider.gameObject);
+
             switch (action)
             {
+                case "open":
+                    doorController.OpenDoor();
+                    ConsoleManager.LogToConsole($"{doorTypeName} door opened.".AsSuccess());
+                    break;
+
+                case "close":
+                    doorController.CloseDoor();
+                    ConsoleManager.LogToConsole($"{doorTypeName} door closed.".AsSuccess());
+                    break;
+
                 case "toggle":
-                    if (activator != null)
-                        await HandleToggle(activator);
-                    else
-                        await HandleToggleController(doorController);
+                    await doorController.ToggleDoor();
+                    ConsoleManager.LogToConsole($"{doorTypeName} door toggled.".AsSuccess());
                     break;
 
                 case "break":
-                    if (activator != null)
-                        await HandleBreak(activator);
-                    else
-                        await HandleBreakController(doorController);
+                    await doorController.BreakDoor();
+                    ConsoleManager.LogToConsole($"{doorTypeName} door broken.".AsSuccess());
                     break;
 
                 default:
-                    ConsoleManager.LogToConsole("Invalid argument. Use 'toggle' or 'break'.".AsError());
+                    ConsoleManager.LogToConsole(Usage.AsError());
                     break;
             }
-        }
-
-        private MonoBehaviour FindActivator(GameObject obj)
-        {
-            var doorActivator = obj.GetComponentInChildren<BaseDoorActivator>();
-            if (doorActivator != null) return doorActivator;
-
-            var gateActivator = obj.GetComponentInChildren<BaseGateActivator>();
-            return gateActivator;
         }
 
         private BaseDoorController FindDoorController(GameObject obj)
         {
-            var controller = obj.GetComponent<BaseDoorController>();
-            if (controller != null) return controller;
+            // First try to get controller from activator using property
+            var activator = obj.GetComponentInChildren<BaseDoorActivator>();
+            if (activator != null && activator.DoorController != null)
+            {
+                return activator.DoorController;
+            }
 
-            controller = obj.GetComponentInChildren<BaseDoorController>();
-            return controller;
+            // Otherwise get controller directly
+            var directController = obj.GetComponent<BaseDoorController>();
+            if (directController != null)
+            {
+                return directController;
+            }
+
+            return obj.GetComponentInChildren<BaseDoorController>();
         }
 
-        private async UniTask HandleToggle(MonoBehaviour activator)
+        private string GetDoorTypeName(GameObject obj)
         {
-            if (activator is BaseDoorActivator doorActivator)
+            var activator = obj.GetComponentInChildren<BaseDoorActivator>();
+            if (activator != null)
             {
-                var controllerField = doorActivator.GetType().GetField("targetDoorController");
-                if (controllerField != null)
-                {
-                    var controller = controllerField.GetValue(doorActivator);
-                    if (controller != null)
-                    {
-                        var method = controller.GetType().GetMethod("ToggleDoor");
-                        if (method != null)
-                        {
-                            string activatorType = doorActivator.GetType().Name.Replace("DoorActivator", "");
-                            ConsoleManager.LogToConsole($"{activatorType} door toggled.".AsSuccess());
-                            await (UniTask)method.Invoke(controller, null);
-                            return;
-                        }
-                    }
-                }
+                return activator.GetType().Name.Replace("DoorActivator", "");
             }
-            else if (activator is BaseGateActivator gateActivator)
-            {
-                var controllerField = gateActivator.GetType().GetField("targetGateController");
-                if (controllerField != null)
-                {
-                    var controller = controllerField.GetValue(gateActivator);
-                    if (controller != null)
-                    {
-                        var method = controller.GetType().GetMethod("ToggleGate");
-                        if (method != null)
-                        {
-                            string activatorType = gateActivator.GetType().Name.Replace("GateActivator", "");
-                            ConsoleManager.LogToConsole($"{activatorType} gate toggled.".AsSuccess());
-                            await (UniTask)method.Invoke(controller, null);
-                            return;
-                        }
-                    }
-                }
-            }
-
-            ConsoleManager.LogToConsole("Could not find controller for activator.".AsError());
-        }
-
-        private async UniTask HandleBreak(MonoBehaviour activator)
-        {
-            if (activator is BaseDoorActivator doorActivator)
-            {
-                var controllerField = doorActivator.GetType().GetField("targetDoorController");
-                if (controllerField != null)
-                {
-                    var controller = controllerField.GetValue(doorActivator);
-                    if (controller != null)
-                    {
-                        var method = controller.GetType().GetMethod("BreakDoor");
-                        if (method != null)
-                        {
-                            string activatorType = doorActivator.GetType().Name.Replace("DoorActivator", "");
-                            ConsoleManager.LogToConsole($"{activatorType} door broken.".AsSuccess());
-                            await (UniTask)method.Invoke(controller, null);
-                            return;
-                        }
-                    }
-                }
-            }
-            else if (activator is BaseGateActivator gateActivator)
-            {
-                var controllerField = gateActivator.GetType().GetField("targetGateController");
-                if (controllerField != null)
-                {
-                    var controller = controllerField.GetValue(gateActivator);
-                    if (controller != null)
-                    {
-                        var method = controller.GetType().GetMethod("ToggleGate");
-                        if (method != null)
-                        {
-                            string activatorType = gateActivator.GetType().Name.Replace("GateActivator", "");
-                            ConsoleManager.LogToConsole($"{activatorType} gate toggled.".AsSuccess());
-                            await (UniTask)method.Invoke(controller, null);
-                            return;
-                        }
-                    }
-                }
-            }
-
-            ConsoleManager.LogToConsole("Could not find controller for activator.".AsError());
-        }
-
-        private async UniTask HandleToggleController(BaseDoorController controller)
-        {
-            var method = controller.GetType().GetMethod("ToggleDoor");
-            if (method != null)
-            {
-                ConsoleManager.LogToConsole($"Cell door toggled.".AsSuccess());
-                await (UniTask)method.Invoke(controller, null);
-            }
-        }
-
-        private async UniTask HandleBreakController(BaseDoorController controller)
-        {
-            var method = controller.GetType().GetMethod("BreakDoor");
-            if (method != null)
-            {
-                ConsoleManager.LogToConsole($"Cell door broken.".AsSuccess());
-                await (UniTask)method.Invoke(controller, null);
-            }
+            return "Cell";
         }
     }
 }
