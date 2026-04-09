5472017d6ff18595d6223a58bc981b5252e29e55
Simoxus <simoxusmanagement@gmail.com>
2026-04-09 00:16:33 -0600

    Add an attribute for Lua modding that allows functions to define their wanted return type

---
+++ Assets/_Project/_Features/Modding/Attributes/LuaReturnAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+[AttributeUsage(AttributeTargets.Method)]
+public class LuaReturnAttribute : Attribute
+{
+    public string Type { get; }
+    public string Description { get; }
+    public LuaReturnAttribute(string type, string description = "")
+    {
+        Type = type;
+        Description = description;
+    }
+}
